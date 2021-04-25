6331bd93f7dbbd4ce084eef2259a76596b9ecead
nileshkatore <katorenilesh@gmail.com>
2021-04-25 22:38:15 +0530

    Initial commit

---
+++ iam-resourceserver/src/main/java/com/iam/resoureceserver/api/iamresourceserver/IamResourceserverApplication.java
@@ -0,0 +1,13 @@
+package com.iam.resoureceserver.api.iamresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamResourceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamResourceserverApplication.class, args);
+	}
+
+}
---
+++ iam-resourceserver/src/test/java/com/iam/resoureceserver/api/iamresourceserver/IamResourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.resoureceserver.api.iamresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamResourceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

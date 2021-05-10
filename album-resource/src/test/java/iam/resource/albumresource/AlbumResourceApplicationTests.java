f4fa50eb709e8d6b29c2adecd5e45bdb4b5d6799
nileshkatore <katorenilesh@gmail.com>
2021-05-10 23:49:38 +0530

    initial commit

---
+++ album-resource/src/test/java/iam/resource/albumresource/AlbumResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.resource.albumresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

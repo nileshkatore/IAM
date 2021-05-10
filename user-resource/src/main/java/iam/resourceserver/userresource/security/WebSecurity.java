da84d0f90a17fc5d281d2eaa9131577226661f36
nileshkatore <katorenilesh@gmail.com>
2021-05-11 01:23:58 +0530

    committ

---
+++ user-resource/src/main/java/iam/resourceserver/userresource/security/WebSecurity.java
@@ -0,0 +1,26 @@
+package iam.resourceserver.userresource.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConvertor());
+
+        http.authorizeRequests()
+          /*          .antMatchers(HttpMethod.GET, "/albumresource/albums")
+                    .hasRole("user")*/
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+    }
+}
---
+++ user-resource/src/test/java/iam/resourceserver/userresource/UserResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.resourceserver.userresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
